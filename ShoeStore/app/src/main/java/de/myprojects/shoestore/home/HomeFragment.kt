c105800ed156fe9f5ed397e73fbd16e31d53c228
AppStreet <sang.nguyen2000@hotmail.de>
2020-12-09 18:27:40 +0100

    replaced recyclerView by adding items programmatically

--- ShoeStore/app/src/main/java/de/myprojects/shoestore/home/HomeFragment.kt
+++ ShoeStore/app/src/main/java/de/myprojects/shoestore/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.*
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.marginTop
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -15,6 +14,7 @@ import androidx.navigation.ui.NavigationUI
 import de.myprojects.shoestore.MainViewModel
 import de.myprojects.shoestore.R
 import de.myprojects.shoestore.databinding.FragmentHomeBinding
+import de.myprojects.shoestore.models.Shoe
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -22,61 +22,39 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val viewModel: MainViewModel by activityViewModels()
-    private lateinit var listAdapter: ShoeListAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.home = this
 
         setHasOptionsMenu(true)
         observeShoes()
-        addEntry()
-        addEntry()
+
 
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initRecyclerView()
-    }
-
-    //I use recyclerview here because it saves me boilerplate code, better architecture etc.
-    private fun initRecyclerView() {
-        listAdapter = ShoeListAdapter()
-
-//        recyclerView.apply {
-//            layoutManager = LinearLayoutManager(this.context)
-//            itemAnimator = DefaultItemAnimator()
-//            adapter = listAdapter
-//        }
-    }
-
-    private fun addEntry(){
-        val myLayout: LinearLayout = binding.linearLayout
-
-      val textView = TextView(this.context)
-        textView.text = ("Hi")
-        textView.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        ).apply {
-            setMargins(0,20,0,0)
-        }
-
-        myLayout.addView(textView)
-    }
 
     private fun observeShoes() {
         viewModel.shoes.observe(viewLifecycleOwner, Observer { shoes ->
-//            listAdapter.setData(shoes)
-            addEntry()
+
+            for (shoe in shoes) {
+                addEntry(shoe)
+            }
+
         })
+    }
+
+
+    private fun addEntry(shoe: Shoe) {
+
+        val textView = TextView(this.context)
+        textView.text = shoe.getName()
+        textView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                .apply { setMargins(10, 100, 0, 0) }
+
+        binding.linearLayout.addView(textView)
     }
 
     fun navigateDetail() {
@@ -91,8 +69,6 @@ class HomeFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         //handles navigate to login -> id's of menuItem and fragment destination needs to be the same
-        return NavigationUI.onNavDestinationSelected(
-            item, requireView().findNavController()
-        ) || super.onOptionsItemSelected(item)
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
